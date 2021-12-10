5edf64177032fb89e58c1e39d5a0a2842e7275e8
Sirote Chaiwattanapong (schaiwat) <schaiwat@cisco.com>
2021-12-10 01:43:16 -0500

    Day 10: Syntax Scoring

---
+++ 2021/Day 10: Syntax Scoring/syntax_scoring.py
@@ -0,0 +1,107 @@
+"""Day 10: Syntax Scoring"""
+
+
+import os
+from collections import Counter
+from functools import reduce
+
+
+INPUT = os.path.join(os.path.dirname(__file__), 'input')
+
+PAIRS = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+    '<': '>',
+}
+
+
+class CorruptedSyntaxScoring:
+
+    points = {
+        ')': 3,
+        ']': 57,
+        '}': 1197,
+        '>': 25137.
+    }
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def score(self):
+        """Return the total syntax error score."""
+        counts = Counter(
+            char
+            for line in self._iter_lines()
+            if (char := self._process(line))
+        )
+        return sum(
+            self.points[char] * count
+            for char, count in counts.items()
+        )
+
+    @staticmethod
+    def _process(line):
+        stack = []
+        for char in line:
+            if closer := PAIRS.get(char):
+                stack.append(closer)
+            else:
+                try:
+                    if stack.pop() != char:
+                        return char
+                except IndexError:
+                    return char
+
+        return None
+
+    def _iter_lines(self):
+        with open(self.filename, encoding='utf-8') as input_file:
+            for line in input_file:
+                yield line.rstrip()
+
+
+class IncompleteSyntaxScoring(CorruptedSyntaxScoring):
+
+    points = {
+        ')': 1,
+        ']': 2,
+        '}': 3,
+        '>': 4,
+    }
+
+    def score(self):
+        scores = [
+            self._score(completion_chars)
+            for line in self._iter_lines()
+            if (completion_chars := self._process(line))
+        ]
+        return sorted(scores)[len(scores) // 2]
+
+    def _score(self, chars):
+        return reduce(lambda v, c: v * 5 + self.points[c], chars, 0)
+
+    @staticmethod
+    def _process(line):
+        stack = []
+        for char in line:
+            if closer := PAIRS.get(char):
+                stack.append(closer)
+            else:
+                try:
+                    if stack.pop() != char:
+                        break
+                except IndexError:
+                    break
+        else:
+            return reversed(stack)
+
+        return None
+
+
+def test_part1():
+    assert CorruptedSyntaxScoring('input').score() == 374061
+
+
+def test_part2():
+    assert IncompleteSyntaxScoring('input').score() == 2116639949
